05dfbc835524ee34bf391276be1f1392bb2424f0
AndreasMuncier <olly_s@hotmail.co.uk>
2017-10-02 15:35:42 +0100

    Add first workshop

---
+++ Workshops/Week1/HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Olly Seber on 02/10/2017.
+//  Copyright © 2017 Olly Seber. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
