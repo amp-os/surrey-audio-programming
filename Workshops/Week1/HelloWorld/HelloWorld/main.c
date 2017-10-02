6d9ff58b5f9276331a724c36ea69f5f3e5c3e5d2
AndreasMuncier <olly_s@hotmail.co.uk>
2017-10-02 15:49:48 +0100

    Changing local path

--- Workshops/Week1/HelloWorld/HelloWorld/main.c
+++ Workshops/Week1/HelloWorld/HelloWorld/main.c
@@ -6,10 +6,10 @@
 //  Copyright © 2017 Olly Seber. All rights reserved.
 //
 
-#include <stdio.h>
+#include <stdio.h> // Required for printf
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) { // Inputs to main
     // insert code here...
     printf("Hello, World!\n");
-    return 0;
+    return 0; // Output of main
 }
