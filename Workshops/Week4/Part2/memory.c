7ab762d621a3d0e71bb7c9b38a45c5176cf496d9
AndreasMuncier <olly_s@hotmail.co.uk>
2017-10-27 10:50:31 +0100

    Finished main tasks
    Former-commit-id: 071b9031dea4f53ead488f54866c05f0525ef32f

--- Workshops/Week4/Part2/memory.c
+++ Workshops/Week4/Part2/memory.c
@@ -5,7 +5,8 @@
 const double g_PI = 3.14159265359;
 const double g_TAU = 2 * g_PI;
 
-void printSinWave( double frequency, int samplingFrequency );
+void printSinWave( double frequency, int samplingFrequency, double **offset );
+void printArray( double *array, int length );
 
 int main( int argc, char *argv[] ) {
 	
@@ -14,14 +15,35 @@ int main( int argc, char *argv[] ) {
 	
 	int sampleFrequency = atoi( argv[ 1 ] );
 	
-	printSinWave( 3.5, sampleFrequency );
+	double *outputArray = calloc( sampleFrequency * 2, sizeof( double ) );
+	double *offset = outputArray;
+	if ( outputArray == NULL )
+		return 2;
 	
+	printSinWave( 3.5, sampleFrequency, &offset );
+	
+	printSinWave( 4, sampleFrequency, &offset );
+	
+	printArray( outputArray, 2 * sampleFrequency );
+	
+	free(outputArray);
 	return 0;
 }
 
-void printSinWave( double frequency, int samplingFrequency ) {
+void printSinWave( double frequency, int samplingFrequency, double **offset ) {
+	static double phase = 0;
+	
 	for ( int i = 0; i < samplingFrequency; ++i ) {
-		printf( "%f\n", sin( fmod( ( g_TAU * frequency * i / samplingFrequency ), g_TAU ) ) );
+		*( ( *offset ) + i ) = sin( fmod( ( g_TAU * frequency * i / samplingFrequency ) + phase, g_TAU ) ) ;
+	}
+	*offset = ( ( *offset ) + samplingFrequency );
+	phase = fmod( ( g_TAU * frequency ) + phase, g_TAU );
+	return;
+}
+
+void printArray( double *array, int length ) {
+	for ( int i = 0; i < length; ++i ) {
+		printf( "%f\n", *( array + i ) );
 	}
 	return;
 }
