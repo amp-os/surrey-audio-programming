b53f1ac437a7d29e4a95db328dc3dab6a430117c
AndreasMuncier <olly_s@hotmail.co.uk>
2017-11-03 10:34:51 +0000

    started week5 part 2

---
+++ Workshops/Week5/Part2/memory.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+const double g_PI = 3.14159265359;
+const double g_TAU = 2 * g_PI;
+
+void printSquareWave( double frequency, int samplingFrequency, double *offset, char *output );
+
+bool writeToFile( const char *fileName, void *ptr, size_t size, size_t nmemb );
+
+int main( int argc, char *argv[] ) {
+    
+    if ( argc != 3 )
+        return 1;
+    
+    int sampleFrequency = atoi( argv[ 1 ] );
+    
+    printf( "Selected sample rate: %d\n", sampleFrequency );
+    
+    FILE *file = fopen( argv[ 2 ], "r" );
+    if ( file == NULL ) {
+    	remove( argv[2] );
+    }
+    else {
+    	fclose ( file );
+    }
+    	
+    
+    double *outputArray = calloc( 1, sizeof( double ) );
+    if ( outputArray == NULL )
+        return 2;
+    
+    printSquareWave( 3.5, sampleFrequency, outputArray, argv[ 2 ] );
+    
+    printf( "File %s was closed successfully!\n", argv[ 2 ] );
+    free( outputArray );
+    return 0;
+}
+
+void printSquareWave( double frequency, int samplingFrequency, double *offset, char *output ) {
+    static double phase = 0;
+    
+    for ( int i = 0; i < 10 * samplingFrequency; ++i ) {
+        
+        for ( int h = 1; h * frequency < samplingFrequency / 2; h += 2 ) {
+            *offset +=  ( sin( fmod( ( g_TAU * h * frequency * i / samplingFrequency ) + phase, g_TAU ) ) / h );
+        }
+        
+        if ( !writeToFile( output, offset, sizeof( double ), sizeof( *offset ) / sizeof( double ) ) ) {
+    		printf( "Could not write to file!\n" );
+    		exit( 4 );
+    	}
+    }
+   
+    phase = fmod( ( g_TAU * frequency ) + phase, g_TAU );
+    return;
+}
+
+bool writeToFile( const char *fileName, void *ptr, size_t size, size_t nmemb ) {
+	FILE *f = fopen( fileName, "a" );
+	if ( f == NULL )
+		return false;
+	
+	if ( fwrite( ptr, size, nmemb, f ) != nmemb )
+		return false;
+	
+	if ( fclose( f ) != 0 )
+		return false;
+	
+	return true;
+}
