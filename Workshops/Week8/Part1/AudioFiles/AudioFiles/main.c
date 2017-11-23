af23ee66e7889a3b424c179d85bb4714c02fb575
AndreasMuncier <olly_s@hotmail.co.uk>
2017-11-23 16:22:26 +0000

    Added delay

--- Workshops/Week8/Part1/AudioFiles/AudioFiles/main.c
+++ Workshops/Week8/Part1/AudioFiles/AudioFiles/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <sndfile.h>
 #include <portaudio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     
@@ -36,7 +37,9 @@ int main(int argc, const char * argv[]) {
         return 3;
     }
     
+    const int delaySize = 20000 * inputInfo.channels;
     float buffer[ bufferSize ];
+    float *delay = calloc( delaySize + inputInfo.channels, sizeof( float ) );
     
     sf_count_t count;
     
@@ -47,13 +50,17 @@ int main(int argc, const char * argv[]) {
         return 4;
     }
     
+    int delayIndex = 0;
+    
     do {
         
-        count = sf_read_float( inputFile, buffer, bufferSize / inputInfo.channels );
+        count = sf_read_float( inputFile, buffer, bufferSize );
         
-//        for ( int i = 0; i < count; ++i ) {
-//            buffer[i] = buffer[i] / (double) 2;
-//        }
+        for ( int i = 0; i < count; ++i ) {
+            delay[ delayIndex ] = buffer[ i ];
+            buffer[ i ] += delay[ ( delayIndex + inputInfo.channels ) % delaySize ];
+            delayIndex = ( delayIndex + 1 ) % delaySize;
+        }
         
         if ( Pa_WriteStream( stream, buffer, count / inputInfo.channels ) != paNoError ) {
             printf( "Issue streaming data!\n" );
@@ -63,8 +70,28 @@ int main(int argc, const char * argv[]) {
             return 5;
         }
         
-        
     } while ( count != 0 );
+    
+    
+    /* Flush delay */
+    for ( int i = 0; i < delaySize; ++i ) {
+        buffer[ i % bufferSize ] = delay[ delayIndex + 1 ];
+        delayIndex = ( delayIndex + 1 ) % delaySize;
+        
+        if ( i % bufferSize == 0 ) {
+            if ( Pa_WriteStream( stream, buffer, ( i % bufferSize ) / inputInfo.channels ) != paNoError ) {
+                printf( "Issue streaming end of delay!\n" );
+                Pa_StopStream( stream );
+                Pa_CloseStream( stream );
+                Pa_Terminate();
+                return 5;
+            }
+        }
+            
+        
+    }
+    
+    free( delay );
     
     sf_close( inputFile );
     
